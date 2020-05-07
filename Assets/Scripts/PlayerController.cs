df5ad5e4d49458f9ab3b53443f02f2388822f18f
palbock90 <plbockmann@gmail.com>
2020-05-07 09:33:16 +0200

    added animations

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    private Animator playerAnimator;
+
+    private bool playerMoving;
+    private Vector2 lastMove;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerMoving = false;
+
+        var horizontalInput = Input.GetAxisRaw("Horizontal");
+        var verticalInput = Input.GetAxisRaw("Vertical");
+
+        if (horizontalInput > 0.5f || horizontalInput < -0.5f)
+        {
+            transform.Translate(new Vector3(horizontalInput * moveSpeed * Time.deltaTime, 0f, 0f));
+            playerMoving = true;
+            lastMove = new Vector2(horizontalInput, 0f);
+        }
+
+        if (verticalInput > 0.5f || verticalInput < -0.5f)
+        {
+            transform.Translate(new Vector3(0f, verticalInput * moveSpeed * Time.deltaTime, 0f));
+            playerMoving = true;
+            lastMove = new Vector2(0f, verticalInput);
+        }
+
+        playerAnimator.SetFloat("MoveX", horizontalInput);
+        playerAnimator.SetFloat("MoveY", verticalInput);
+        playerAnimator.SetBool("PlayerMoving", playerMoving);
+        playerAnimator.SetFloat("LastMoveX", lastMove.x);
+        playerAnimator.SetFloat("LastMoveY", lastMove.y);
+    }
+}
