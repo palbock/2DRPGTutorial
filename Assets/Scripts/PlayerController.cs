aa2a5968b121fa57bb9da9189401d5375ce39cc2
palbock90 <plbockmann@gmail.com>
2020-05-14 08:51:44 +0200

    Added tilemaps and collision

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
+
     private Animator playerAnimator;
+    private Rigidbody2D myRigidBody;
 
     private bool playerMoving;
     private Vector2 lastMove;
@@ -14,6 +16,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerAnimator = GetComponent<Animator>();
+        myRigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -26,16 +29,25 @@ public class PlayerController : MonoBehaviour
 
         if (horizontalInput > 0.5f || horizontalInput < -0.5f)
         {
-            transform.Translate(new Vector3(horizontalInput * moveSpeed * Time.deltaTime, 0f, 0f));
+            myRigidBody.velocity = new Vector2(horizontalInput * moveSpeed, myRigidBody.velocity.y);
             playerMoving = true;
             lastMove = new Vector2(horizontalInput, 0f);
         }
 
         if (verticalInput > 0.5f || verticalInput < -0.5f)
         {
-            transform.Translate(new Vector3(0f, verticalInput * moveSpeed * Time.deltaTime, 0f));
+            myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, verticalInput * moveSpeed);
             playerMoving = true;
             lastMove = new Vector2(0f, verticalInput);
+        }
+
+        if(horizontalInput == 0)
+        {
+            myRigidBody.velocity = new Vector2(0f, myRigidBody.velocity.y);
+        }
+        if (verticalInput == 0)
+        {
+            myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, 0f);
         }
 
         playerAnimator.SetFloat("MoveX", horizontalInput);
